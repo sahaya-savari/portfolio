a17b07c6e64478e2e17ec119c10075c136d66d79
Sahaya Savari F <172002578+sahaya-savari@users.noreply.github.com>
2026-06-19 01:01:34 +0530

    perf: dynamically load framer-motion domAnimation to eliminate 1s TBT during hydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { LazyMotion, domAnimation } from "framer-motion";
+import { LazyMotion } from "framer-motion";
 import "./index.css";
 import App from "./App";
 
+// Dynamically import domAnimation to eliminate TBT during hydration
+const loadFeatures = () => import("framer-motion").then(res => res.domAnimation);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LazyMotion features={domAnimation}>
+    <LazyMotion features={loadFeatures}>
       <App />
     </LazyMotion>
   </StrictMode>
