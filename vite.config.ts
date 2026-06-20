71dd8eb4f222256a4f7608db696443704d741297
Sahaya Savari F <172002578+sahaya-savari@users.noreply.github.com>
2026-06-20 17:19:24 +0530

    fix: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/portfolio/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
