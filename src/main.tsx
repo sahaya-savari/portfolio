49e13a1582ebbebc18c654b1a34289d7eee50b14
Sahaya Savari F <172002578+sahaya-savari@users.noreply.github.com>
2026-09-12 23:26:36 +0530

    SEO: prevent duplicate head metadata

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App";
 
+if (typeof document !== 'undefined') {
+  document.querySelectorAll('head > [data-rh="true"]').forEach((el) => el.remove());
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
